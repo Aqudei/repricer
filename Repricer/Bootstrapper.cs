d60b339a7fb97b727012bbdce103e7f45c63799d
Aqudei <fdeu.aalmine@gmail.com>
2019-12-01 12:24:48 +0800

    Initial

---
+++ Repricer/Bootstrapper.cs
@@ -0,0 +1,47 @@
+﻿using Caliburn.Micro;
+using Repricer.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Unity;
+
+namespace Repricer
+{
+    class Bootstrapper : BootstrapperBase
+    {
+        private readonly IUnityContainer _unityContainer = new Unity.UnityContainer();
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _unityContainer.BuildUp(instance);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _unityContainer.ResolveAll(service);
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _unityContainer.Resolve(service, key);
+        }
+
+        protected override void Configure()
+        {
+            _unityContainer.RegisterSingleton<IWindowManager, WindowManager>();
+            _unityContainer.RegisterSingleton<IEventAggregator, EventAggregator>();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
